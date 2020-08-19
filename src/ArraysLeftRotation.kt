75f6f3ff11929dedf560e05e92073a96d41e0e08
poornima <poornima.tharma@anywhere.co>
2020-08-19 11:09:31 +0530

    ArraysLeftRotation

---
+++ src/ArraysLeftRotation.kt
@@ -0,0 +1,48 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the rotLeft function below.
+fun rotLeft(a: Array<Int>, d: Int): ArrayList<Int> {
+    var result = arrayListOf<Int>()
+    var rotation = d
+    while (result.size != a.size) {
+        result.add(a[rotation])
+        rotation = rotation.inc()
+        if (rotation > a.size - 1) {
+            rotation = 0
+        }
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val nd = scan.nextLine().split(" ")
+
+    val n = nd[0].trim().toInt()
+
+    val d = nd[1].trim().toInt()
+
+    val a = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = rotLeft(a, d)
+
+    println(result.joinToString(" "))
+}
